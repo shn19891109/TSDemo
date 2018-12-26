2a4e61eab92d9c0e6421faa7d14e15dbabcebfb3
apple <apple@appledeiMac-7.local>
2018-12-26 16:50:39 +0800

    初始化

---
+++ Example/Pods/Target Support Files/TSDemo/TSDemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_TSDemo : NSObject
+@end
+@implementation PodsDummy_TSDemo
+@end
---
+++ TSDemo/Classes/categorys/NSDate+Judge.h
@@ -0,0 +1,23 @@
+//
+//  NSDate+Judge.h
+//  IMUITest
+//
+//  Created by ma c on 2018/9/6.
+//  Copyright © 2018年 ma c. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDate (Judge)
+///** 是否是今天 */
+//- (BOOL)chat_isInToDay;
+//
+///** 是否是明天 */
+//- (BOOL)chat_isInTomorrow;
+//
+///** 是否是昨天 */
+//- (BOOL)chat_isInYesterday;
+//
+///** 是否是今年 */
+//- (BOOL)chat_isInThisYear;
+@end
